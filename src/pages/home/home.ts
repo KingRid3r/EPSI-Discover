4f57f0fcc5527ce1bc2a3ecb2e70508b150fc456
Luca Benezit <33320379+KingRid3r@users.noreply.github.com>
2018-10-04 13:12:51 +0200

    VR Stable

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,13 +6,15 @@ import { TutorialPage } from '../tuto/tuto';
 import { Platform } from "ionic-angular";
 import { AndroidPermissions } from '@ionic-native/android-permissions';
 import { Diagnostic } from '@ionic-native/diagnostic';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController, public modalCtrl: ModalController, private storage: Storage, public platform: Platform,private diagnostic : Diagnostic, private androidPermissions : AndroidPermissions ) {
+
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController, private storage: Storage, public platform: Platform,private diagnostic : Diagnostic, private androidPermissions : AndroidPermissions, private camera : Camera ) {
     if(this.platform.is('android'))
     {
       this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.CAMERA).then(
@@ -20,7 +22,7 @@ export class HomePage {
         err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.CAMERA)
       );
 
-      this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.CAMERA, this.androidPermissions.PERMISSION.GET_ACCOUNTS]);
+      this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.CAMERA, this.androidPermissions.PERMISSION.GET_ACCOUNTS]).then(success => console.log(success), err => console.log(err));
     }
 
     storage.get('tuto').then((val) => {
@@ -31,5 +33,23 @@ export class HomePage {
       }
     });
   }
+  permission(){
+    this.diagnostic.getPermissionAuthorizationStatus(this.diagnostic.permission.CAMERA).then((status) => {
+      console.log(`AuthorizationStatus`);
+      console.log(status);
+      if (status != this.diagnostic.permissionStatus.GRANTED) {
+        this.diagnostic.requestRuntimePermission(this.diagnostic.permission.CAMERA).then((data) => {
+          console.log(`getCameraAuthorizationStatus`);
+          console.log(data);
+        })
+      } else {
+        console.log("We have the permission");
+      }
+    }, (statusError) => {
+      console.log(`statusError`);
+      console.log(statusError);
+    });
+  }
+
 
 }
